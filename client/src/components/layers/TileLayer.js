e38966852ee2464466d9fb89bdb8a4cefc3b25b2
crogers <creedrogers@gmail.com>
2021-09-24 22:38:10 -0400

    refactor continues...

---
+++ client/src/components/layers/TileLayer.js
@@ -0,0 +1,29 @@
+import { useEffect, useContext } from 'react';
+import OLTileLayer from "ol/layer/Tile";
+
+import MapContext from '../MapContext';
+
+const TileLayer = ({ source, zIndex = 0 }) => {
+    const { map } = useContext(MapContext);
+    useEffect(() => {
+        if (!map) return;
+
+        let tileLayer = new OLTileLayer({
+            source,
+            zIndex
+        });
+        map.addLayer(tileLayer);
+        tileLayer.setZIndex(zIndex);
+
+    
+        return () => {
+            if (map) {
+                map.removeLayer(tileLayer);
+            }
+        };
+    }, [map]);
+
+    return null;
+};
+
+export default TileLayer;
